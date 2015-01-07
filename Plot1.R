14da0f656b70b4ae569fe5baae01d3cc39ee6f3e
yuvalsher <yuval.sherman@gmail.com>
2015-01-07 18:14:33 +0200

    Add first script too

---
+++ Plot1.R
@@ -0,0 +1,29 @@
+library(data.table)
+library(dplyr)
+
+# Download end extract the data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipfile <- "household_power_consumption.zip"
+txtfile <- "household_power_consumption.txt"
+download.file(fileUrl,destfile=zipfile,method="curl")
+unzip(zipfile, files=txtfile, overwrite=TRUE)
+
+# Load the data into memory
+colclasses <- c("factor", "factor", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+fulldata <- read.table(txtfile, header = TRUE, sep=";", colClasses = colclasses, na.strings = "?")
+
+#filter only the data for the two days we need.
+data <- filter(fulldata, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Reclaim the memory for the full data
+rm(fulldata)
+
+# Merge the date and time to a new column.
+data$DateTime <- strptime( paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+# Make sure the dates are printed as expected.
+Sys.setlocale("LC_TIME", "English")
+
+# Do plot 1
+png(filename="plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col="red", xlab="Global Active Power", main="Global Active Power")
+dev.off()
